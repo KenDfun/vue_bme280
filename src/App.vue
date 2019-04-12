24e14ab55159a878ffd05539ba628f7624ea13f3
Ken Okabe <ken@design-fun.com>
2019-04-13 07:12:25 +0900

    addd cool design for date & information!

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <Bme280Api msg="API BME280" :info="info"/>
+    <Bme280Api msg="API BME280" :info="info" :date="date"/>
   </div>
 </template>
 
 <script>
 import Bme280Api from './components/Bme280Api.vue'
 import axios from 'axios'
+import { setInterval } from 'timers';
 
 export default {
   name: 'app',
@@ -15,14 +16,22 @@ export default {
   },
   data () {
     return {
-      info: ""
+      info: "",
+      date: new Date()
     }
   },
   mounted () {
+    this.getBme280()
+    setInterval(() => this.getBme280(), 1000)
+  },
+  methods: {
+    getBme280() {
     axios
       .get('http://localhost:3000/api/bme280')
-      .then(response => (this.info = response.data))
-  }
+      .then(response => (this.info = response.data));
+    this.date = new Date()
+    }
+  },
 }
 </script>
 
--- src/components/Bme280Api.vue
+++ src/components/Bme280Api.vue
@@ -2,18 +2,31 @@
   <div class="bme280api">
     <h1>{{ msg }}</h1>
     <p>{{info}}</p>
-    <p>温度：{{tempData}}℃</p>
-    <p>気圧：{{pressureData}}HP</p>
-    <p>湿度：{{humidityData}}%</p>
+
+    <div class="container">
+      <p class="date">{{ year }}/{{ month }}/{{ day }} {{hours}}:{{minutes}}.{{seconds}}</p>
+      <div class="data280">
+      <p class="data280-item temp-degree">{{tempData}}</p>
+      <p class="data280-item press-hpa">{{pressureData}}</p>
+      <p class="data280-item humid-percent">{{humidityData}}</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+
+const zeroPadding = (num, digit) => {
+  return (Array(digit).join("0") + num).slice(-digit)
+}
+
 export default {
   name: 'Bme280Api',
   props: {
     msg: String,
-    info: {}
+    info: {},
+    date: null
   },
   computed:{
     tempData: function(){
@@ -24,6 +37,24 @@ export default {
     },
     humidityData: function(){
       return this.info.humidity;
+    },
+    year() {
+      return this.date.getFullYear()
+    },
+    month() {
+      return zeroPadding(this.date.getMonth() + 1, 2)
+    },
+    day() {
+      return zeroPadding(this.date.getDate(), 2)
+    },
+    hours() {
+      return zeroPadding(this.date.getHours(), 2)
+    },
+    minutes() {
+      return zeroPadding(this.date.getMinutes(), 2)
+    },
+    seconds() {
+      return zeroPadding(this.date.getSeconds(), 2)
     },
   }
 }
@@ -45,4 +76,72 @@ li {
 a {
   color: #42b983;
 }
+
+.container {
+  background-color: #3a4a5e;
+  padding: 2%;
+}
+ 
+ 
+.date {
+  text-align: right;
+  color: #fff;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 1.5rem;
+  letter-spacing: .1em;
+  margin: .0em 0;
+  line-height: 1;
+}
+ 
+ 
+.data280 {
+  display: flex;
+}
+ 
+ 
+.data280-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1;
+  height: 85px;
+  position: relative;
+  z-index: 1;
+  padding: 0.5em;
+  margin: 3px;
+  color: #fff;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 3rem;
+  line-height: 1;
+  background-color: #48b883;
+  box-sizing: border-box;
+}
+ 
+ 
+.data280-item:before {
+  position: absolute;
+  right: 5px;
+  bottom: 1px;
+  z-index: 1;
+  color: #3a4a5e;
+  font-family: 'Teko', sans-serif;
+  font-size: 1.4rem;
+  letter-spacing: .05em;
+}
+ 
+ 
+.temp-degree:before {
+  content: "degree";
+}
+ 
+ 
+.press-hpa:before {
+  content: "hPa";
+}
+ 
+ 
+.humid-percent:before {
+  content: "%";
+}
+
 </style>
